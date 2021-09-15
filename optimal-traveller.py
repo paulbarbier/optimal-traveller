12114f018ee7c980fd8a5c1bf7b54b8f9b5a0b01
Paul Barbier <paulb24@orange.fr>
2021-09-15 17:30:17 +0200

    Add comments

--- optimal-traveller.py
+++ optimal-traveller.py
@@ -6,13 +6,16 @@ import sys
 from optimal_traveller.cli import cli
 
 def main():
+    # Create a parser
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers(dest = "command")
 
+    # Create commands
     load_parser = subparser.add_parser("load")
     solve_parser = subparser.add_parser("solve")
     display_parser = subparser.add_parser("display")
 
+    # Add arguments to commands
     load_parser.add_argument("-f", "--filename", help = "FILENAME.txt or FILENAME.json to be loaded in the solver", type = str, required = True)
     load_parser.add_argument("-m", "--metric", help = "metric used to compute the distance matrix: euclidian, ...", type = str, required = True)
 
@@ -22,12 +25,15 @@ def main():
 
     display_parser.add_argument("-s", "--solution", help = "SOLUTION is the name of the solution to be displayed",type = str, required = True)
 
+    # Print the help menu when there is no typed commands
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(0)
 
+    # Parse arguments
     arguments = parser.parse_args()
 
+    # Create a cli object to handle commands
     app = cli()
 
     if arguments.command == "load":
