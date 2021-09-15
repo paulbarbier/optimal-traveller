80df32795c88ec40a7a258a8d8aed7c283480d64
Hannes Van Overloop <hannes.van-overloop@eleves.enpc.fr>
2021-09-15 16:52:29 +0200

    json_treatment_v1

--- optimal_traveller/modules/loader.py
+++ optimal_traveller/modules/loader.py
@@ -1,4 +1,23 @@
+import json
+import numpy as np
+
+class city:
+    def __init__(self, name, latitude, longitude):      #initializer
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+
 class loader:
-    tbtb
     def __init__(self):
         pass
+
+    def load_json(self, file_adress):                   #loads the data from a json file
+        f = open(str(file_adress), "r")                 #opens the file
+        data = json.load(f)                             #converts it into a string
+        f.close()
+
+        city_list = []
+        for location in data:                           #for every city we get its name, latitude, longitude
+            city_list.append(city(location('capital'), location('lat'), location('long')))
+
+        self.city_list = np.city_list()                 #eventually we get a tabular with the cities' data
