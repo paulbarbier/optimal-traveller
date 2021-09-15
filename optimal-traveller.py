e09b648f4ac41b2f97540c6479277897a2eef68d
Paul Barbier <paulb24@orange.fr>
2021-09-15 17:17:05 +0200

    Modify the parser code in order to include options for different commands

--- optimal-traveller.py
+++ optimal-traveller.py
@@ -3,35 +3,40 @@
 import argparse
 import sys
 
-from optimal_traveller import cli
+from optimal_traveller.cli import cli
 
 def main():
     parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest = "command")
 
-    action = parser.add_mutually_exclusive_group()
+    load_parser = subparser.add_parser("load")
+    solve_parser = subparser.add_parser("solve")
+    display_parser = subparser.add_parser("display")
 
-    action.add_argument("-l", "--load", help = "load a *.txt or *.json file into the solver", action = "store")
-    action.add_argument("-s", "--solve", help = "compute an optimal path with the given method", action = "store")
-    action.add_argument("-d", "--display", help = "display a previously computed path", action = "store")
+    load_parser.add_argument("-f", "--filename", help = "FILENAME.txt or FILENAME.json to be loaded in the solver", type = str, required = True)
+    load_parser.add_argument("-m", "--metric", help = "metric used to compute the distance matrix: euclidian, ...", type = str, required = True)
+
+    solve_parser.add_argument("-g", "--graph", help="name of the cities graph previously computed by the load command",
+                              type=str, required=True)
+    solve_parser.add_argument("-m", "--method", help = "select the method used to solve: ", type = str, required = True)
+
+    display_parser.add_argument("-s", "--solution", help = "SOLUTION is the name of the solution to be displayed",type = str, required = True)
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(0)
 
-    try:
-        args = parser.parse_args()
-    except:
-        print("unvalid command !")
-        parser.print_help()
-        sys.exit(0)
+    arguments = parser.parse_args()
 
-    if args.load:
-        print("load is working !")
-        print(args.load)
-    if args.solve:
-        print("solve is working !")
-    if args.display:
-        print("display is working !")
+    app = cli()
+
+    if arguments.command == "load":
+        app.load(arguments.filename, arguments.metric)
+    elif arguments.command == "solve":
+        app.solve(arguments.graph, arguments.method)
+    elif arguments.command == "display":
+        app.display(arguments.solution)
+
 
 if __name__ == "__main__":
     main()
--- optimal_traveller/cli.py
+++ optimal_traveller/cli.py
@@ -4,13 +4,9 @@ from optimal_traveller.modules import loader, solver, graphic
 class cli:
     def __init__(self):
         pass
-
-    def load(filename):
-        pass
-
-    def solve():
-        pass
-
-    def display():
-        pass
-
+    def load(self, filename, metric):
+        print("load")
+    def solve(self, graph, method):
+        print("solve")
+    def display(self, solution):
+        print("display")
