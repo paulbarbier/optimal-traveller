8eb62b94b5bb0788a9e7716a3449cf19d5ee43f5
Hannes Van Overloop <hannes.van-overloop@eleves.enpc.fr>
2021-09-15 18:22:49 +0200

    json_v3

--- optimal_traveller/modules/loader.py
+++ optimal_traveller/modules/loader.py
@@ -8,16 +8,25 @@ class city:
         self.longitude = longitude
 
 class loader:
-    def __init__(self):
-        pass
+    def __init__(self, city_list):
+        self.cities_list = np.array(city_list)
 
     def load_json(self, file_adress):                   #loads the data from a json file
         f = open(str(file_adress), "r")                 #opens the file
         data = json.load(f)                             #converts it into a string
+        print("json data loaded")
         f.close()
 
         city_list = []
         for location in data:                           #for every city we get its name, latitude, longitude
-            city_list.append(city(location('capital'), float(location('lat')), float(location('long')))
+            city_list.append(city(location['capital'], float(location['lat']), float(location['long'])))
+        print("data explored")
 
-        self.city_list = np.city_list()                 #eventually we get a tabular with the cities' data
+        self.cities_list = np.array(city_list)          #eventually we get a tabular with the cities' data
+        print("data put into tabular")
+
+def main():
+    print("main started")
+    test = loader.load_json("~/2A/projet_rentree/optimal-traveller/data/capitals.json")
+    print("data loaded")
+    print(test.cities_list)
