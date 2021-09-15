8e7b73167ef62837fc16ffe3729bd20508663b95
Prunelle Vogler <prunelle-vogler@orange.fr>
2021-09-15 17:05:19 +0200

    fonction_matrice_distances

--- optimal_traveller/modules/loader.py
+++ optimal_traveller/modules/loader.py
@@ -1,3 +1,16 @@
 class loader:
     def __init__(self):
         pass
+
+import numpy as np
+
+def distances(list):
+    nb_villes = len(list)
+    W = np.zeros(nb_villes, nb_villes)
+    for i in range(0, nb_villes):
+        for j in range(0, i):
+            if i != j:
+                W[i][j] = np.sqrt((list[i].latitude - list[j].latitude)*(list[i].latitude - list[j].latitude) + (list[i].longitude - list[j].longitude)*(list[i].longitude - list[j].longitude))
+                W[j][i] = np.sqrt((list[i].latitude - list[j].latitude)*(list[i].latitude - list[j].latitude) + (
+                        list[i].longitude - list[j].longitude)*(list[i].longitude - list[j].longitude))
+    return W
