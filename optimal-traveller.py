8196666aa046b1a3af314a02f4adaa1e83b8a208
Hannes Van Overloop <hannes.van-overloop@eleves.enpc.fr>
2021-09-15 16:20:20 +0200

    affichage commande invalide

--- optimal-traveller.py
+++ optimal-traveller.py
@@ -9,12 +9,12 @@ sys.path.append("/modules/")
 
 def main():
     parser = argparse.ArgumentParser()
-    
+
     action = parser.add_mutually_exclusive_group()
 
     action.add_argument("-l", "--load", help = "load a *.txt or *.json file into the solver", action = "store")
     action.add_argument("-s", "--solve", help = "compute an optimal path with the given method", action = "store")
-    action.add_argument("-d", "--display", help = "display a previously computed path", action = "store")
+    action.add_argument("-d", "--display",- help = "display a previously computed path", action = "store")
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -23,16 +23,17 @@ def main():
     try:
         args = parser.parse_args()
     except:
+        print("unvalid command !")
         parser.print_help()
         sys.exit(0)
 
     if args.load:
-        print("load is working!")
+        print("load is working !")
         print(args.load)
     if args.solve:
-        print("solve is working!")
+        print("solve is working !")
     if args.display:
-        print("display is working!")
+        print("display is working !")
 
 if __name__ == "__main__":
     main()
