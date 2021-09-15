eb548263f8b51159eb05da35232caee88b9036f3
Hannes Van Overloop <hannes.van-overloop@eleves.enpc.fr>
2021-09-15 20:07:15 +0200

    correction syntaxique

--- optimal-traveller.py
+++ optimal-traveller.py
@@ -5,10 +5,11 @@ import sys
 
 from optimal_traveller.cli import cli
 
+
 def main():
     # Create a parser
     parser = argparse.ArgumentParser()
-    subparser = parser.add_subparsers(dest = "command")
+    subparser = parser.add_subparsers(dest="command")
 
     # Create commands
     load_parser = subparser.add_parser("load")
@@ -16,14 +17,14 @@ def main():
     display_parser = subparser.add_parser("display")
 
     # Add arguments to commands
-    load_parser.add_argument("-f", "--filename", help = "FILENAME.txt or FILENAME.json to be loaded in the solver", type = str, required = True)
-    load_parser.add_argument("-m", "--metric", help = "metric used to compute the distance matrix: euclidian, ...", type = str, required = True)
+    load_parser.add_argument("-f", "--filename", help="FILENAME.txt or FILENAME.json to be loaded in the solver", type=str, required=True)
+    load_parser.add_argument("-m", "--metric", help="metric used to compute the distance matrix: euclidian, ...", type=str, required=True)
 
     solve_parser.add_argument("-g", "--graph", help="name of the cities graph previously computed by the load command",
                               type=str, required=True)
-    solve_parser.add_argument("-m", "--method", help = "select the method used to solve: ", type = str, required = True)
+    solve_parser.add_argument("-m", "--method", help="select the method used to solve: ", type=str, required=True)
 
-    display_parser.add_argument("-s", "--solution", help = "SOLUTION is the name of the solution to be displayed",type = str, required = True)
+    display_parser.add_argument("-s", "--solution", help="SOLUTION is the name of the solution to be displayed", type=str, required=True)
 
     # Print the help menu when there is no typed commands
     if len(sys.argv) == 1:
--- optimal_traveller/cli.py
+++ optimal_traveller/cli.py
@@ -2,10 +2,13 @@ from optimal_traveller.modules.loader import loader
 from optimal_traveller.modules.solver import solver
 from optimal_traveller.modules.graphic import graphic
 
-class cli:
+
+class CLI:
     def load(self, filename, metric):
         print("load")
+
     def solve(self, graph, method):
         print("solve")
+
     def display(self, solution):
         print("display")
