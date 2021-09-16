9c5cb33bccf27a75f56988bca83e26e142f24592
Paul Barbier <paulb24@orange.fr>
2021-09-16 10:23:21 +0200

    Add json compatibility to Loader class

--- optimal_traveller/modules/loader.py
+++ optimal_traveller/modules/loader.py
@@ -1,33 +1,52 @@
 import json
-import numpy as np
-
-
-class City:
-    def __init__(self, name, latitude, longitude):  # initializer
-        self.name = name
-        self.latitude = latitude
-        self.longitude = longitude
+from math import sqrt
 
 
 class Loader:
     def __init__(self):
+        self.data = {}
+
+    def load_txt(self, filename):
         pass
 
-    def load_json(self, file_adress):                   # loads the data from a json file
-        f = open(str(file_adress), "r")                 # opens the file
-        data = json.load(f)                             # converts it into a string
-        f.close()
+    def load_json(self, filename, metric):
+        with open(filename) as json_file:
+            input_json = json.load(json_file)
 
-        city_list = []
-        for location in data:  # for every city we get its name, latitude, longitude
-            city_list.append(City(data[location]['capital'], float(data[location]['lat']), float(data[location]['long'])))
+            self.data = {"settings": {"metric": metric,
+                                      "filename": filename,
+                                      "number_cities": 0},
+                         "cities": [],
+                         "weight_matrix": [],
+                         "solutions": []}
 
-        self.cities_list = np.array(city_list)  # eventually we get a tabular with the cities' data
+            i = 1
+            for location in input_json:
+                self.data["cities"].append({"id": i,
+                                            "name": input_json[location]['capital'],
+                                            "latitude": float(input_json[location]['lat']),
+                                            "longitude": float(input_json[location]['long'])})
+                i += 1
 
-    def distances(self):
-        nb_villes = len(self.cities_list)
-        self.W = np.zeros(nb_villes, nb_villes)
-        for i in range(0, nb_villes):
-            for j in range(0, i):
-                self.W[i][j] = np.sqrt((self.cities_list[i].latitude - self.cities_list[j].latitude)*(self.cities_list[i].latitude - self.cities_list[j].latitude) + (self.cities_list[i].longitude - self.cities_list[j].longitude)*(self.cities_list[i].longitude - self.cities_list[j].longitude))
-                self.W[j][i] = self.W[i][j]
+            self.data["settings"]["number_cities"] = i-1
+
+            self.data["weight_matrix"] = [[0 for k in range(i-1)] for l in range(i-1)]
+
+    def read_json(self, filename):
+        with open(filename, "r") as json_file:
+            self.data = json.load(json_file)
+
+    def write_json(self, filename):
+        with open(filename, "w") as json_file:
+            json_file.truncate(0)
+            json.dump(self.data, json_file)
+
+    def compute_euclidean_distances(self):
+        row_number = 0
+        for city_row in self.data["cities"]:
+            column_number = 0
+            for city_column in self.data["cities"][:row_number]:
+                self.data["weight_matrix"][row_number][column_number] = sqrt((city_row["latitude"] - city_column["latitude"])**2 + (city_row["longitude"] - city_column["longitude"])**2)
+                self.data["weight_matrix"][column_number][row_number] = self.data["weight_matrix"][row_number][column_number]
+                column_number += 1
+            row_number += 1
