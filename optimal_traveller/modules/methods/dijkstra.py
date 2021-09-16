6a28064d17c471e2f137fbc628a3f37c0a4874fb
Hannes Van Overloop <hannes.van-overloop@eleves.enpc.fr>
2021-09-16 18:00:49 +0200

    name change

--- optimal_traveller/modules/methods/dijkstra.py
+++ optimal_traveller/modules/methods/dijkstra.py
@@ -1,34 +1,34 @@
-class Dijkstra:
+class NearestNeighbour:
     def __init__(self):
         pass
 
     # Fonctions auxiliaires_Dijkstra
-    def choix_plus_proche_voisin(self, W, noeuds_restants, noeud_actuel):
+    def choix_plus_proche_voisin(self, w, noeuds_restants, noeud_actuel):
         min = noeuds_restants[0]
         for i in range(1, len(noeuds_restants)):
-            if W[noeud_actuel][noeuds_restants[i]] < W[noeud_actuel][min]:
+            if w[noeud_actuel][noeuds_restants[i]] < w[noeud_actuel][min]:
                 min = noeuds_restants[i]
         return min
 
-    def supprime(self, Liste, noeud):
+    def supprime(self, liste, noeud):
         indice = 0
-        for i in range(0, len(Liste)):
-            if Liste[i] == noeud:
+        for i in range(0, len(liste)):
+            if liste[i] == noeud:
                 indice = i
-        del Liste[indice]
+        del liste[indice]
 
     # Fonction presque finale
-    def Dijkstra(self, W, Noeuds_restants, Noeud_depart):
-        nb_villes = len(Noeuds_restants)
-        Ordre = [Noeud_depart]
-        self.supprime(Noeuds_restants, Noeud_depart)
-        while len(Ordre) < nb_villes:
-            new_noeud = self.choix_plus_proche_voisin(W, Noeuds_restants, Ordre[-1])
-            Ordre.append(new_noeud)
-            self.supprime(Noeuds_restants, new_noeud)
+    def nearest_neighbour(self, weight, noeuds_restants, noeud_depart):
+        nb_villes = len(noeuds_restants)
+        solution = [noeud_depart]
+        self.supprime(noeuds_restants, noeud_depart)
+        while len(solution) < nb_villes:
+            new_noeud = self.choix_plus_proche_voisin(weight, noeuds_restants, solution[-1])
+            solution.append(new_noeud)
+            self.supprime(noeuds_restants, new_noeud)
 
         for i in range(nb_villes):
-            Ordre[i]+=1
-        Ordre.append(Ordre[0])
-        print(Ordre)
-        return Ordre
+            solution[i]+=1
+        solution.append(solution[0])
+        print(solution)
+        return solution
--- optimal_traveller/modules/solver.py
+++ optimal_traveller/modules/solver.py
@@ -1,4 +1,4 @@
-from methods.dijkstra import Dijkstra
+from methods.dijkstra import NearestNeighbour
 # from modules.methods.exact import Exact
 from loader import Loader
 
@@ -8,12 +8,12 @@ class Solver:
 
     # Fonction finale qui ajoute la solution dans data
 
-    def solver_dijkstra(self, data, start_city):
+    def solver_nn(self, data, start_city):
         W = data["weight_matrix"]
         nb_cities = data['settings']['number_cities']
         Liste = [i for i in range(nb_cities)]
-        solution = Dijkstra()
-        Ordre = solution.Dijkstra(W, Liste, start_city)
+        solution = NearestNeighbour()
+        Ordre = solution.nearest_neighbour(W, Liste, start_city)
         data['solutions'].append({"method": "Dijkstra", "resulting_path": Ordre})
 
     def exact_solver(self):
@@ -26,7 +26,7 @@ def main():
     test.read_json('test.opt')
     set = test.data
     new = Solver()
-    new.solver_dijkstra(set, 8)
+    new.solver_nn(set, 8)
 
 
 main()
