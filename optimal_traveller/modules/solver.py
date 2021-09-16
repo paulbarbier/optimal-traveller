a6b02093439275adf73f284d9fb4edb7f176c3e9
Paul Barbier <paulb24@orange.fr>
2021-09-16 11:55:19 +0200

    move dijkstra code of Prunelle to dijsktra.py

--- optimal_traveller/modules/solver.py
+++ optimal_traveller/modules/solver.py
@@ -6,52 +6,7 @@ class Solver:
     def __init__(self):
         pass
 
-    #####Fonctions auxiliaires_Dijkstra
-    def distances_entre(self, W, city1, city2):
-        return W[city1][city2]
-
-    def choix_plus_proche_voisin(self, Liste, poids):
-        min = 0
-        for city in Liste:
-            if poids[min] > poids[city]:
-                min = city
-        return (min)
-
-    def supprime(self, Liste, noeud):
-        indice = 0
-        for i in range(0, len(noeud)):
-            if Liste[i] == noeud:
-                indice = i
-        del Liste[i]
-
-    def init_poids(self, Noeud_départ, Liste_villes):
-        n = len(Liste_villes)
-        poids = []
-        for i in range(0, n):
-            if i == Noeud_départ:
-                poids[i] = 0
-            else:
-                poids[i] = float("inf")
-        return poids
-
-    #Fonction presque finale
-    def Dijkstra(self, W, Liste_villes, Noeud_départ):
-        En_cours = []
-        Ordre = []
-        poids = init_poids(Noeud_départ, Liste_villes)
-        En_cours.append(Noeud_départ)
-        while len(En_cours) != 0:
-            new_noeud = choix_plus_proche_voisin(En_cours, poids)
-            Ordre.append(new_noeud)
-            supprime(Liste_villes, new_noeud)
-            for city in Liste_villes:
-                if poids[city] > poids[new_noeud] + distances_entre(W, city, new_noeud):
-                    poids[city] = poids[new_noeud] + distances_entre(W, city, new_noeud)
-                    En_cours.append(city)
-            supprime(En_cours, new_noeud)
-        return Ordre
-
-    #Fonction finale qui ajoute la solution dans data
+    # Fonction finale qui ajoute la solution dans data
     def Solver_Dijkstra(self):
         W = data["weight-matrix"]
         Liste = data['cities']
