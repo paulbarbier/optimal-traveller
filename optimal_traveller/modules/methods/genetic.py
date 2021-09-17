8137b1d96b0065ccb3b24155c815c2360a96d138
Hannes Van Overloop <hannes.van-overloop@eleves.enpc.fr>
2021-09-17 12:54:32 +0200

    heuristic genetique marche

--- optimal_traveller/modules/methods/genetic.py
+++ optimal_traveller/modules/methods/genetic.py
@@ -12,23 +12,21 @@ class Genetic:
 
     def fitness(self, path_ind):
         fitness = 0
-        for i in range(len(self.population[path_ind])-1):
-            fitness += self.weight_matrix[self.population[path_ind][i]][self.population[path_ind][i+1]]
+        for i in range(1, len(self.population[path_ind])-1):
+            fitness += self.weight_matrix[self.population[path_ind][i]-1][self.population[path_ind][i+1]-1]
         self.population[path_ind][0] = fitness
 
     def random_path_generator(self, nodes):
         path = [0]
-        path.append(nodes[-1])
-        nodes.remove(-1)
+        path.append(nodes.pop())
 
         while len(nodes) != 0:
             indice = random.randrange(len(nodes))
-            path.append(nodes[indice])
-            nodes.remove(indice)
+            path.append(nodes.pop(indice))
 
         path.append(path[1])
         self.population.append(path)
-        self.fitness(-1)
+        self.fitness(len(self.population)-1)
 
     # def path_crosser(self, path1, path2, weight):
     #    indice1 = random.randrange(len(self.population[path1]))
@@ -55,25 +53,37 @@ class Genetic:
         return self.population[path_ind][0]
 
     def sort_population(self):
-        self.population.sort(key=self.give_fitness)
+        for i in range(1,len(self.population)):
+            ind = i
+            while ind > 0 and self.population[ind][0] < self.population[ind-1][0]:
+                self.population[ind], self.population[ind-1] = self.population[ind-1], self.population[ind]
+                ind -= 1
 
     def select_population(self):
         limit = int(self.percentage*len(self.population))
         for i in range(limit):
-            self.population.remove(-1)
+            self.population.pop(-1)
 
     def population_generator(self, nodes):
         for i in range(self.size):
-            self.random_path_generator(nodes)
+            self.random_path_generator(nodes.copy())
 
-    def solve_genetic(self, nodes):
-        self.population_generator(nodes)
+    def solve(self, nodes):
+        self.population_generator(nodes.copy())
         self.sort_population()
         self.select_population()
         iteration = 1
         while iteration < self.steps:
             while len(self.population) <= self.size:
-                self.path_mutation(self.population[random.randrange(len(self.population))])
+                self.path_mutation(random.randrange(len(self.population)))
             self.sort_population()
             self.select_population()
             iteration += 1
+        self.resulting_path = self.population[0][1:]
+
+
+W = [[0, 2, 3, 4, 5], [4, 0, 1, 6, 4], [5, 2, 0, 4, 6], [1, 1, 1, 0, 2], [3, 4, 2, 4, 0]]
+
+test = Genetic(0.5, 10, 5, W)
+test.solve([1,2,3,4,5])
+print(test.resulting_path)
