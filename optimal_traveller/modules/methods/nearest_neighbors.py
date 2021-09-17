0b65254f9d1a866460815e0e9ad587fbe33c8b6b
Paul Barbier <paulb24@orange.fr>
2021-09-17 10:34:53 +0200

    Add nearest-neighbors method to optimal-traveller.py and clean NearestNeighbors class

--- optimal_traveller/modules/methods/nearest_neighbors.py
+++ optimal_traveller/modules/methods/nearest_neighbors.py
@@ -1,34 +1,40 @@
+import random
+
+
 class NearestNeighbors:
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.weight_matrix = data["weight_matrix"]
+        self.number_cities = data["settings"]["number_cities"]
+        self.remaining_cities = [i for i in range(self.number_cities)]
+        self.number_remaining_cities = self.number_cities
+        self.resulting_path = []
 
-    # Fonctions auxiliaires_Dijkstra
-    def choix_plus_proche_voisin(self, w, noeuds_restants, noeud_actuel):
-        min = noeuds_restants[0]
-        for i in range(1, len(noeuds_restants)):
-            if w[noeud_actuel][noeuds_restants[i]] < w[noeud_actuel][min]:
-                min = noeuds_restants[i]
-        return min
+    def get_nearest_neighbour(self):
+        minimum = self.remaining_cities[0]
+        for i in range(1, len(self.remaining_cities)):
+            if self.weight_matrix[self.resulting_path[-1]][self.remaining_cities[i]] < \
+                    self.weight_matrix[self.resulting_path[-1]][minimum]:
+                minimum = self.remaining_cities[i]
+        return minimum
 
-    def supprime(self, liste, noeud):
-        indice = 0
-        for i in range(0, len(liste)):
-            if liste[i] == noeud:
-                indice = i
-        del liste[indice]
+    def delete_node(self, node):
+        index = 0
+        for i in range(len(self.remaining_cities)):
+            if self.remaining_cities[i] == node:
+                index = i
+                break
+        del self.remaining_cities[index]
+        self.number_remaining_cities -= 1
 
-    # Fonction presque finale
-    def nearest_neighbour(self, weight, noeuds_restants, noeud_depart):
-        nb_villes = len(noeuds_restants)
-        solution = [noeud_depart]
-        self.supprime(noeuds_restants, noeud_depart)
-        while len(solution) < nb_villes:
-            new_noeud = self.choix_plus_proche_voisin(weight, noeuds_restants, solution[-1])
-            solution.append(new_noeud)
-            self.supprime(noeuds_restants, new_noeud)
+    def solve(self):
+        random_start_node = random.choice(self.remaining_cities)
+        self.resulting_path.append(random_start_node)
+        self.delete_node(random_start_node)
+        while len(self.resulting_path) < self.number_remaining_cities:
+            new_node = self.get_nearest_neighbour()
+            self.resulting_path.append(new_node)
+            self.delete_node(new_node)
 
-        for i in range(nb_villes):
-            solution[i]+=1
-        solution.append(solution[0])
-        print(solution)
-        return solution
+        for city in self.resulting_path:
+            city += 1
+        self.resulting_path.append(self.resulting_path[0])
